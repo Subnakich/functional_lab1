ffdbe3e40e298a37db5a046d6b31ea3e86f98721
yar56 <subnak@list.ru>
2023-10-12 09:58:53 +0300

    init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,20 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+//    println("Hello World!")
+//
+//    // Try adding program arguments via Run/Debug configuration.
+//    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+//    println("Program arguments: ${args.joinToString()}")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val list = listOf(1, 2, 2, 3, 4, 4, 4, 5, 5, 6)
+    val n = 2
+    val result = filterElementsByFrequency(list, n)
+    println("Элементы, встречающиеся более $n раз: $result")
+}
+
+fun <T> filterElementsByFrequency(list: List<T>, n: Int): List<T> {
+    // Создаем мапу для подсчета частоты встречаемости элементов
+    val elementCountMap = list.groupingBy { it }.eachCount()
+
+    // Фильтруем элементы списка, оставляя только те, которые встречаются более n раз
+    return list.filter { (elementCountMap[it] ?: 0) > n }
 }
